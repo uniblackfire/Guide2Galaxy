49ae3642570ad10a535b15c9ac74bb1932c82fb6
uniblackfire <uniblackfire@gmail.com>
2016-09-04 01:17:53 +0800

    fix bug in money_credit_relation init method

--- src/parsed_data/money_credit_relation.py
+++ src/parsed_data/money_credit_relation.py
@@ -6,11 +6,9 @@ from util import translator
 class money_credit_relation(IntergalacticEarthRelation):
     def __init__(self, data, word_roman_relation_list):
         pattern = re.compile(r'(.+)\s+(\w+)\s+is\s+(\d+)\s+Credits')
-        m = re.match(pattern, data, re.IGNORECASE)
+        m = re.match(pattern, data)
 
-        self.word_roman_relation_list = word_roman_relation_list
-
-        self.number = translator.translate_alien_numerals_to_arabic_numerals(m.group(1), self.word_roman_relation_list)
+        self.number = translator.translate_alien_numerals_to_arabic_numerals(m.group(1), word_roman_relation_list)
         self.unit = m.group(2)
         self.value = int(m.group(3))
 
