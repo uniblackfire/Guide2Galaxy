599e9fd143795eb741672f76bafec88132d1a668
uniblackfire <uniblackfire@gmail.com>
2016-09-04 00:53:24 +0800

    fix bug in word_roman_relation init method

--- src/parsed_data/word_roman_relation.py
+++ src/parsed_data/word_roman_relation.py
@@ -6,9 +6,10 @@ from parsed_data.intergalactic_earth_relation import IntergalacticEarthRelation
 
 class word_roman_relation(IntergalacticEarthRelation):
     def __init__(self, data):
-        data_split = data.split('is')
-        self.word = data_split[0].strip()
-        self.roman = data_split[1].strip()
+        pattern = re.compile(r'(\w+)\s+is\s+([IVXLCDM])')
+        m = re.match(pattern, data)
+        self.word = m.group(1)
+        self.roman = m.group(2)
 
     def get_info(self):
         return self.word, self.roman
--- src/util/translator.py
+++ src/util/translator.py
@@ -1,11 +1,8 @@
-import re
-
 from util.roman import getRomanNum
 
 
 def translate_alien_numerals_to_arabic_numerals(input_data, word_roman_relation_list):
-    pattern = re.compile(r'\s+')
-    input_data = re.split(pattern, input_data)
+    input_data = input_data.split()
 
     word_roman_dict = dict()
     for item in word_roman_relation_list:
