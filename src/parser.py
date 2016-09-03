3e065c9d5fd546437b257155c27a281d59164bc6
uniblackfire <uniblackfire@gmail.com>
2016-09-03 19:00:56 +0800

    modify parse method

--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,21 @@
 import re
 
-from constants import WORD_ROMAN_RELATION
+from constants import *
+
+PATTERN_STRINGS = [
+    (r'\w+\s+is\s+[IVXLCDM]', WORD_ROMAN_RELATION),
+    (r'.+\s+is\s+\d+\s+Credits', MONEY_CREDIT_RELATION),
+    (r'how\s+much\s+is\s+.+\?', HOW_MUCH_QUESTION),
+    (r'how\s+many\s+Credits\s+is\s+.+\?', HOW_MANY_QUESTION)
+]
+
+
+def get_re_pattern(pattern_string):
+    return re.compile(pattern_string, re.IGNORECASE)
 
 
 def parse(input_data):
-    pattern_string = r'\w+\s+is\s+[IVXLCDM]'
-    pattern = re.compile(pattern_string, re.IGNORECASE)
-    if re.match(pattern, input_data):
-        return WORD_ROMAN_RELATION
-
+    for ps in PATTERN_STRINGS:
+        pattern = get_re_pattern(ps[0])
+        if re.match(pattern, input_data):
+            return ps[1]
