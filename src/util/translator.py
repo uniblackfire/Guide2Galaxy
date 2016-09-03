2020f49491be174e581faa996c5970ec46858660
uniblackfire <uniblackfire@gmail.com>
2016-09-04 00:46:55 +0800

    complete translate_alien_numerals_to_arabic_numerals

--- src/util/translator.py
+++ src/util/translator.py
@@ -1,3 +1,18 @@
-def translate_alien_numerals_to_arabic_numerals(input_data, word_roman_relation_list):
+import re
 
-    return None
+from util.roman import getRomanNum
+
+
+def translate_alien_numerals_to_arabic_numerals(input_data, word_roman_relation_list):
+    pattern = re.compile(r'\s+')
+    input_data = re.split(pattern, input_data)
+
+    word_roman_dict = dict()
+    for item in word_roman_relation_list:
+        word_roman_dict[item.get_info()[0]] = item.get_info()[1]
+
+    roman_number = ''
+    for word in input_data:
+        roman_number += word_roman_dict[word]
+
+    return getRomanNum(roman_number)
