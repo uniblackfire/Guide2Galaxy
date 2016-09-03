aa6e6b40fc439016cd5633a2c47a20ac6afbb39b
uniblackfire <uniblackfire@gmail.com>
2016-09-03 18:18:06 +0800

    update parser test cases

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,6 +1,6 @@
 import unittest
 
-from constants import WORD_ROMAN_RELATION
+from constants import WORD_ROMAN_RELATION, MONEY_CREDIT_RELATION, HOW_MUCH_QUESTION, HOW_MANY_QUESTION
 from parsed_data import parser
 
 
@@ -12,3 +12,27 @@ class TestParser(unittest.TestCase):
         result = parser.parse(input_data)
         # then
         self.assertIs(result, WORD_ROMAN_RELATION)
+
+    def test_parse_money_credit_relation(self):
+        # given
+        input_data = 'glob glob Silver is 34 Credits'
+        # when
+        result = parser.parse(input_data)
+        # then
+        self.assertIs(result, MONEY_CREDIT_RELATION)
+
+    def test_parse_how_much_question(self):
+        # given
+        input_data = 'how much is pish tegj glob glob ?'
+        # when
+        result = parser.parse(input_data)
+        # then
+        self.assertIs(result, HOW_MUCH_QUESTION)
+
+    def test_parse_how_much_question(self):
+        # given
+        input_data = 'how many Credits is glob prok Silver ?'
+        # when
+        result = parser.parse(input_data)
+        # then
+        self.assertIs(result, HOW_MANY_QUESTION)
