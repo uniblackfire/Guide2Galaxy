3088b693d89a677b9961921ae46cf3f71ae764f3
uniblackfire <uniblackfire@gmail.com>
2016-09-03 18:13:39 +0800

    update parser test cases

--- src/parsed_data/parser.py
+++ src/parsed_data/parser.py
@@ -1,2 +1,8 @@
+import re
+
+
 def parse():
+    pattern_string = ''
+    pattern = re.compile(pattern_string, re.IGNORECASE)
+
     return None
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,13 +1,14 @@
 import unittest
 
+from constants import WORD_ROMAN_RELATION
 from parsed_data import parser
 
 
 class TestParser(unittest.TestCase):
-    def test_parse_method(self):
+    def test_parse_word_roman_relation(self):
         # given
         input_data = 'tegj is L'
         # when
         result = parser.parse()
         # then
-        self.assertIsNotNone(result)
+        self.assertIs(result, WORD_ROMAN_RELATION)
