71556d06be34d0137f59e00c593c491a17da010d
uniblackfire <uniblackfire@gmail.com>
2016-09-03 19:04:50 +0800

    update parser

--- src/parser.py
+++ src/parser.py
@@ -2,13 +2,6 @@ import re
 
 from constants import *
 
-PATTERN_STRINGS = [
-    (r'\w+\s+is\s+[IVXLCDM]', WORD_ROMAN_RELATION),
-    (r'.+\s+is\s+\d+\s+Credits', MONEY_CREDIT_RELATION),
-    (r'how\s+much\s+is\s+.+\?', HOW_MUCH_QUESTION),
-    (r'how\s+many\s+Credits\s+is\s+.+\?', HOW_MANY_QUESTION)
-]
-
 
 def get_re_pattern(pattern_string):
     return re.compile(pattern_string, re.IGNORECASE)
