8199de0305a0dbc2b6028e9b05aaa233c91557b5
uniblackfire <uniblackfire@gmail.com>
2016-09-03 13:03:40 +0800

    create test case

---
+++ test/test_input.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest import mock
+
+
+
+
+
+class TestInput(unittest.TestCase):
+    def test_read_file(self):
+        # given
+        filename = './data/input.txt'
+        # when
+        content = Manager.read_file(filename)
+        # then
+        assert content is not None
