a23a84795f1e50acd7d404a47b5c280764334f41
uniblackfire <uniblackfire@gmail.com>
2016-09-03 13:26:52 +0800

    edit test case

--- test/test_input.py
+++ test/test_input.py
@@ -1,12 +1,14 @@
 import unittest
 from unittest import mock
+import os
 
+import file
 import src.manager as manager
 
 class TestInput(unittest.TestCase):
     def test_read_file(self):
         # given
-        filename = '/Users/jiayizhang/code/PycharmProjects/Guide2Galaxy/data/input.txt'
+        filename = os.path.join(file.project_dir, 'data/input.txt')
         # when
         content = manager.read_file(filename)
         # then
