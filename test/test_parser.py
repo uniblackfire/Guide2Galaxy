bb52490d50ccbdb248788a0bfe571aa7f3c41308
uniblackfire <uniblackfire@gmail.com>
2016-09-04 10:00:29 +0800

    update test_parser.py

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import mock
+from unittest.mock import patch
 
+import manager
 import parser
 from constants import WORD_ROMAN_RELATION, MONEY_CREDIT_RELATION, HOW_MUCH_QUESTION, HOW_MANY_QUESTION
 from parsed_data.money_credit_relation import money_credit_relation
@@ -8,6 +10,19 @@ from parsed_data.word_roman_relation import word_roman_relation
 
 
 class TestParser(unittest.TestCase):
+    def setUp(self):
+        word_roman_relation_a = word_roman_relation('glob is I')
+        word_roman_relation_b = word_roman_relation('prok is V')
+        word_roman_relation_c = word_roman_relation('pish is X')
+        word_roman_relation_d = word_roman_relation('tegj is L')
+        word_roman_relation_list = [word_roman_relation_a,
+                                    word_roman_relation_b,
+                                    word_roman_relation_c,
+                                    word_roman_relation_d]
+        self.word_roman_relation_dict = dict()
+        for item in word_roman_relation_list:
+            self.word_roman_relation_dict[item.get_info()[0]] = item.get_info()[1]
+
     def test_word_roman_relation_init_field(self):
         # given
         input_data = 'tegj is L'
@@ -28,10 +43,9 @@ class TestParser(unittest.TestCase):
     def test_money_credit_relation_init_field(self):
         # given
         input_data = 'glob glob Silver is 34 Credits'
-        word_roman_relation_a = word_roman_relation('glob is I')
-        word_roman_relation_list = [word_roman_relation_a]
         # when
-        obj = money_credit_relation(input_data, word_roman_relation_list)
+        with patch.dict(manager.word_roman_relation_dict, self.word_roman_relation_dict):
+            obj = money_credit_relation(input_data)
         # then
         self.assertEqual(obj.number, 2)
         self.assertEqual(obj.unit, 'Silver')
