3ef91db08270ae5e11f54c87d140aeb5925a30bc
uniblackfire <uniblackfire@gmail.com>
2016-09-04 14:45:42 +0800

    update how_much_question

--- src/parsed_data/how_much_question.py
+++ src/parsed_data/how_much_question.py
@@ -9,5 +9,4 @@ class how_much_question(Question):
 
         alien_number = re.match(pattern, self.data).group(1)
         arabic_number = translator.translate_alien_numerals_to_arabic_numerals(alien_number)
-        return alien_number + ' is ' \
-               + str(arabic_number)
+        return alien_number + ' is ' + str(arabic_number)
