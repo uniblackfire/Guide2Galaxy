7bb964a08c867f4abd513c6096b366843a050810
uniblackfire <uniblackfire@gmail.com>
2016-09-04 09:54:22 +0800

    update translate_alien_numerals_to_arabic_numerals

--- src/util/translator.py
+++ src/util/translator.py
@@ -7,8 +7,9 @@ def translate_alien_numerals_to_arabic_numerals(input_data):
 
     roman_number = ''
     for word in input_data:
-        # try:
-        roman_number += manager.word_roman_relation_dict[word]
-        # except:
-        #     return None
+        try:
+            roman_number += manager.word_roman_relation_dict[word]
+        except KeyError:
+            print(KeyError)
+            return None
     return getRomanNum(roman_number)
